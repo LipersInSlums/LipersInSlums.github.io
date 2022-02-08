54564249c01ce241c7b35c1a25a4ebc8feb5625b
Lugendre <lugendre1904@gmail.com>
2022-02-08 22:58:43 +0900

    fix: disable next/image

--- next.config.js
+++ next.config.js
@@ -1,48 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const securityHeaders = [
-  {
-    key: "X-DNS-Prefetch-Control",
-    value: "on",
-  },
-  {
-    key: "Strict-Transport-Security",
-    value: "max-age=63072000; includeSubDomains; preload",
-  },
-  {
-    key: "X-XSS-Protection",
-    value: "1; mode=block",
-  },
-  {
-    key: "X-Frame-Options",
-    value: "SAMEORIGIN",
-  },
-  {
-    key: "Permissions-Policy",
-    value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
-  },
-  {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
-  },
-  {
-    key: "Referrer-Policy",
-    value: "origin-when-cross-origin",
-  },
-  // CSPに関してはStrict CSPを実現するために `_document.tsx` に記述する．
-];
-
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: "/:path*",
-        headers: securityHeaders,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
