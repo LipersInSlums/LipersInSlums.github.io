cfe0126d4a66c9bf5e99870f52765847f38953b4
happou31 <happo31@gmail.com>
2022-02-12 02:11:04 +0900

    feat: add OGP 出すやつ

---
+++ src/components/common/PostOGP.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+
+type OGPPageType = "article" | "blog";
+
+type Props = {
+  title: string;
+  type: OGPPageType;
+  description: string;
+  url: string;
+};
+
+const PostOGP: React.VFC<Props> = ({
+  title,
+  type,
+  description,
+  url,
+}: Props) => (
+  <Head>
+    <meta property="og:title" content={`${title} - LipersInSlums Wiki`} />
+    <meta property="og:type" content={type} />
+    <meta property="og:url" content={url} />
+    <meta property="og:description" content={description} />
+  </Head>
+);
+
+export default PostOGP;
