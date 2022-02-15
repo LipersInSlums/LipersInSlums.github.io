853b676b0a41fd0a9f1b18d16f3ae2181c7b626c
Mitama <yussa.de.dannann@gmail.com>
2022-02-16 02:42:25 +0900

    feat: theme.common

--- styles/theme.ts
+++ styles/theme.ts
@@ -3,32 +3,36 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#95A540',
-      light: '#C0C67A',
-      dark: '#7A8446',
+      main: "#95A540",
+      light: "#C0C67A",
+      dark: "#7A8446",
     },
     secondary: {
-      main: '#A98546',
-      light: '#6F4A2A',
-      dark: '#404A4A',
+      main: "#A98546",
+      light: "#6F4A2A",
+      dark: "#404A4A",
     },
   },
 });
 
-declare module '@mui/material/styles' {
+declare module "@mui/material/styles" {
   interface Theme {
     palette: {
+      common: {
+        black: "#000";
+        white: "#fff";
+      };
       primary: {
-        main: '#95A540',
-        light: '#C0C67A',
-        dark: '#7A8446',
-      },
+        main: "#95A540";
+        light: "#C0C67A";
+        dark: "#7A8446";
+      };
       secondary: {
-        main: '#A98546',
-        light: '#6F4A2A',
-        dark: '#404A4A',
-      },
-    },
+        main: "#A98546";
+        light: "#6F4A2A";
+        dark: "#404A4A";
+      };
+    };
   }
 }
 
