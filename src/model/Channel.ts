38da9eeb54fbf2c37ca51af1d8e854fff7ac2c2c
happo31 <happo31@gmail.com>
2024-12-08 11:16:32 +0900

    feat: チャンネル説明を md から生成する準備

--- src/model/Channel.ts
+++ src/model/Channel.ts
@@ -1,11 +1,20 @@
-export type ChannelInfo = {
+export type ChannelInfo = ChannelInfoBySlug | ChannelInfoByConst;
+type ChannelBase = {
+  ignoreList?: boolean;
   name: string;
   topic: string;
   description: string;
   since: string;
-  notes: string[];
   refs: {
     name: string;
     href: string;
   }[];
+};
+
+type ChannelInfoBySlug = ChannelBase & {
+  md?: `_channels/${string}.md`;
+};
+
+type ChannelInfoByConst = ChannelBase & {
+  notes: string[];
 };
